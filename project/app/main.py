4569b78fc471b3ac78941b084fe25c54c2851bdf
Sugumar Srinivasan <72388581+SugumarSrinivasan@users.noreply.github.com>
2025-05-23 08:54:59 +0530

    Update main.py

--- project/app/main.py
+++ project/app/main.py
@@ -1,13 +1,19 @@
+"""This module contains the main application logic."""
+
 def add(a, b):
+    """This function does addition of two numbers."""
     return a + b
 
 def subtract(a, b):
+    """This function does subraction of two numbers."""
     return a - b
 
 def multiply(a, b):
+    """This function does multiplication of two numbers."""
     return a * b
 
 def divide(a, b):
+    """This function does division of two numbers."""
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
